c1fd9cdc806c6fb73f1e3348ab93859afe8b8ea7
Ricard Jonker <a40527@alunos.ipb.pt>
2020-05-23 11:07:25 +0100

    testing

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -7,8 +7,25 @@ namespace maxiorphy\star_rating;
  */
 class AutoloadExample extends \yii\base\Widget
 {
-    public function run()
+    public function run($rating)
     {
+        $rate = round($rating);
+        for ($i = 0; $i < $rate - 1; $i++) {
+            echo '<i class="fa fa-star gold-star"></i>';
+        }
+        if ($rate - $rating >= 0.5) {
+            echo '<i class="fa fa-star-half-alt gold-star" ></i>';
+        } elseif ($rating == 0) {
+        } elseif ($rate - $rating <= 0.5) {
+            echo '<i class="fa fa-star gold-star" ></i>';
+        } else if ($rate - $rating >= 0 && $rating != 0) {
+            echo '<i class="fa fa-star gold-star" ></i>';
+        } else {
+            echo '<i class="fa fa-star grey-star" ></i>';
+        }
+        for ($i = 0; $i < 5 - $rate; $i++) {
+            echo '<i class="fa fa-star grey-star"></i>';
+        }
         echo "Hello!";
     }
 }
