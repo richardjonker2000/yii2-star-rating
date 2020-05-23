f0adc83bebd27a780f8e245ff16ec1a7ffa253fe
Ricard Jonker <a40527@alunos.ipb.pt>
2020-05-23 10:53:46 +0100

    testing

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -7,7 +7,7 @@ namespace maxiorphy\star_rating;
  */
 class AutoloadExample extends \yii\base\Widget
 {
-    public function run()
+    public static function run()
     {
         return "Hello!";
     }
